50d39b776427daa088de1d270bca54d7879ba606
Tiana Newton-White <tiananewtonwhite@gmail.com>
2023-11-12 14:36:57 -0500

    Practice exercies from javascript Part I Course Material

---
+++ independent-javascript-practice/incrementing.js
@@ -0,0 +1,8 @@
+//Increment and Decrement will be practiced below.
+
+let myNumber = 42;
+var num1 = (++myNumber) + (myNumber++);
+console.log(myNumber);
+console.log(num1);
+
+//Questions to ask. Why does myNumber not increment again even though I typed console.log again on line 5 before printing num1?
---
+++ independent-javascript-practice/javascript-part1.js
@@ -0,0 +1,25 @@
+let x = 1;
+x +=2;
+console.log(x);
+
+let y = 1;
+y *= 2;
+console.log(y);
+
+let w = 1;
+w -= 3;
+console.log(w);
+
+let num1 = 8;
+num1 % 4;
+console.log(num1);
+
+
+
+//converts variable to string
+let num5 =8;
+num6 = num5.toString();
+console.log(num6);
+
+console.log(typeof(num6));
+
