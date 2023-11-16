d70ee2fb7888dfe5687b01351ff875c4ffcd6ece
Tiana Newton-White <tiananewtonwhite@gmail.com>
2023-11-16 16:47:23 -0500

    Adding progress for For Loops Exercise JavaScript I this is a WIP

---
+++ js/for_loops.js
@@ -0,0 +1,20 @@
+//For Loops Exercise 11/16/2023
+//Exercise #2 ForLoops
+function showMultiplicationTable(number) {
+    for (let i = 1; i <11; i++) {
+    console.log(`${number} * ${i} = ${number * i}`);
+}}
+showMultiplicationTable(7)
+
+//Exercise #3 For Loops
+//Exercise #4 For Loops
+//Exercise #5 For Loops
+for (let i = 100; i >=5  ; i-=5) {
+    console.log(`${i}`);
+    if (i === 0) {
+        break;
+    }
+}
+
+
+
