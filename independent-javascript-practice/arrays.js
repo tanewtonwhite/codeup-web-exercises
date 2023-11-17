9348934d7f9682171048bceca5dfdaf9482f3dff
Tiana Newton-White <tiananewtonwhite@gmail.com>
2023-11-17 05:03:32 -0500

    independent arrays practice

---
+++ independent-javascript-practice/arrays.js
@@ -0,0 +1,10 @@
+//Independent Array Practice
+let colors = ['blue', 'green', 'orange', 'indigo'];
+let colors2 = ['cyan', 'yellow', 'black'];
+console.log(colors);
+console.log(colors2);
+
+console.log(colors.length);
+console.log(colors2.length);
+
+
